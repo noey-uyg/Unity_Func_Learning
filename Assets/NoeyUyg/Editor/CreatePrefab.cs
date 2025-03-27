5b950e2c2820b852422b6798326404b7f541437f
noey-uyg <dnlrbdus31@gmail.com>
2025-03-27 18:22:14 +0900

    - modify : 에디터 프리팹 생성 기능 수정

--- Assets/NoeyUyg/Editor/CreatePrefab.cs
+++ Assets/NoeyUyg/Editor/CreatePrefab.cs
@@ -30,36 +30,38 @@ public class CreatePrefab
 
     //Create
    #region Fruits_Create
-   [MenuItem("GameObject/Prefabs/Fruits/Apple ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/1/Apple",false, 1)]
    public static void CreateApple() => CreatePrefabs("Apple");
-   [MenuItem("GameObject/Prefabs/Fruits/Banana ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/1/Banana",false, 1)]
    public static void CreateBanana() => CreatePrefabs("Banana");
-   [MenuItem("GameObject/Prefabs/Fruits/Blueberry ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/1/Blueberry",false, 1)]
    public static void CreateBlueberry() => CreatePrefabs("Blueberry");
-   [MenuItem("GameObject/Prefabs/Fruits/Cherry ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/2/Cherry",false, 1)]
    public static void CreateCherry() => CreatePrefabs("Cherry");
-   [MenuItem("GameObject/Prefabs/Fruits/Coconut ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/2/Coconut",false, 1)]
    public static void CreateCoconut() => CreatePrefabs("Coconut");
-   [MenuItem("GameObject/Prefabs/Fruits/Lime ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/2/Lime",false, 1)]
    public static void CreateLime() => CreatePrefabs("Lime");
-   [MenuItem("GameObject/Prefabs/Fruits/Melon ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/3/Melon",false, 1)]
    public static void CreateMelon() => CreatePrefabs("Melon");
-   [MenuItem("GameObject/Prefabs/Fruits/Orange ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/3/Orange",false, 1)]
    public static void CreateOrange() => CreatePrefabs("Orange");
-   [MenuItem("GameObject/Prefabs/Fruits/Peach ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/3/Peach",false, 1)]
    public static void CreatePeach() => CreatePrefabs("Peach");
-   [MenuItem("GameObject/Prefabs/Fruits/Pineapple ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/3/Pineapple",false, 1)]
    public static void CreatePineapple() => CreatePrefabs("Pineapple");
-   [MenuItem("GameObject/Prefabs/Fruits/Watermelon ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Fruits/3/Watermelon",false, 1)]
    public static void CreateWatermelon() => CreatePrefabs("Watermelon");
    #endregion
 
    #region Panel_Create
-   [MenuItem("GameObject/Prefabs/Panel/GameoverPanel ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Panel/GameoverPanel",false, 1)]
    public static void CreateGameoverPanel() => CreatePrefabs("GameoverPanel");
-   [MenuItem("GameObject/Prefabs/Panel/Play_BG ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Panel/Play_BG",false, 1)]
    public static void CreatePlay_BG() => CreatePrefabs("Play_BG");
-   [MenuItem("GameObject/Prefabs/Panel/Title_BG ",false, 1)]
+   [MenuItem("GameObject/Prefabs/Panel/Title_BG",false, 1)]
    public static void CreateTitle_BG() => CreatePrefabs("Title_BG");
    #endregion
+
+
 }
